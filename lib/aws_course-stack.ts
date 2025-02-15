e7920a350e23ed84592c38f1d2b3a7fce98d82dd
dkapczynska <dominikakapczynska1@gmail.com>
2025-02-15 19:55:11 +0100

    modified:   lib/aws_course-stack.ts 	new file:   myshop_app/nodejs-aws-shop-react 	new file:   package-lock.json 	modified:   package.json

--- lib/aws_course-stack.ts
+++ lib/aws_course-stack.ts
@@ -1,4 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +9,31 @@ export class AwsCourseStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const myshop_bucket = new s3.Bucket(this, 'cloud-dev-task2-automated-dep-20250215', {
+      versioned: true,
+      blockPublicAccess: {
+        blockPublicAcls: false,
+        blockPublicPolicy: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      bucketName: 'cloud-dev-task2-automated-dep-20250215'
+  });
+  
+  myshop_bucket.addToResourcePolicy(new iam.PolicyStatement({
+    sid: 'PublicReadGetObject',
+    effect: iam.Effect.ALLOW,
+    principals: [new iam.AnyPrincipal()],
+    actions: ['s3:GetObject'],
+    resources: [`${myshop_bucket.bucketArn}/*`]
+  }));
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCourseQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
+  new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+    sources: [s3deploy.Source.asset('./myshop_app/nodejs-aws-shop-react/dist')],
+    destinationBucket: myshop_bucket,
+    destinationKeyPrefix: '/'
+  });
+
+}};
